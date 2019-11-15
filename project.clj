ef18dd6bec7d3dbdc21ba61263f39e7a5375a903
Miikka Koskinen <miikka@metosin.fi>
2019-11-15 07:47:29 +0200

    Fork it

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.cemerick/url "0.1.2-SNAPSHOT"
+(defproject miikka/url "0.1.2-SNAPSHOT"
   :description "Makes working with URLs in Clojure a little more pleasant."
-  :url "http://github.com/cemerick/url"
+  :url "http://github.com/miikka/url"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
